8015a800dde621741039d677b19b126e8796a2dc
GavinZzw <18792981374@163.com>
2017-08-09 22:52:23 +0800

    add my blog

---
+++ my_blog/article/migrations/0001_initial.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Article',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('title', models.CharField(max_length=100)),
+                ('category', models.CharField(max_length=50, blank=True)),
+                ('date_time', models.DateTimeField(auto_now_add=True)),
+                ('content', models.TextField(null=True, blank=True)),
+            ],
+            options={
+                'ordering': ['-date_time'],
+            },
+        ),
+    ]
